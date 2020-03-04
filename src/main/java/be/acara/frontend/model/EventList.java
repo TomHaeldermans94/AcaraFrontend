dd186b5b06a236a050942a6e52c15df7ff7a590e
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-03-04 21:33:40 +0100

    Extend base version of the project architecture

--- src/main/java/be/acara/frontend/model/EventList.java
+++ src/main/java/be/acara/frontend/model/EventList.java
@@ -1,4 +1,14 @@
 package be.acara.frontend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventList {
+    private List<Event> eventList;
 }
