6469c601ef238182ad9c7061aeef4a1060d7e855
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-03-22 00:20:34 +0100

    AC-21 add and edit events with fileupload

--- src/main/java/be/acara/frontend/config/WebConfig.java
+++ src/main/java/be/acara/frontend/config/WebConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource() {
@@ -22,5 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource());
         return bean;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1000000);
+        return multipartResolver;
     }
 }
--- src/main/java/be/acara/frontend/controller/EventController.java
+++ src/main/java/be/acara/frontend/controller/EventController.java
@@ -2,27 +2,38 @@ package be.acara.frontend.controller;
 
 import be.acara.frontend.model.Event;
 import be.acara.frontend.model.EventList;
+import be.acara.frontend.model.EventWithoutImage;
 import be.acara.frontend.service.EventFeignClient;
+import be.acara.frontend.service.mapper.EventMapper;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
     
-    private EventFeignClient eventFeignClient;
+    private final EventFeignClient eventFeignClient;
+    private final EventMapper mapper;
+    private Log logger = LogFactory.getLog(this.getClass());
 
     @Autowired
-    public EventController(EventFeignClient eventFeignClient) {
+    public EventController(EventFeignClient eventFeignClient, EventMapper mapper) {
         this.eventFeignClient = eventFeignClient;
+        this.mapper = mapper;
     }
     
     @GetMapping("/detail/{id}")
@@ -44,35 +55,55 @@ public class EventController {
 
     @GetMapping("/new")
     public String displayAddEventForm(Model model){
-        model.addAttribute("categoryList", eventFeignClient.getAllCategories().getCategories());
         model.addAttribute("event", new Event());
         return "addEvent";
     }
 
     @PostMapping("/new")
-    public String handleAddEventForm(@Valid @ModelAttribute("event") Event event, BindingResult br) {
+    public ModelAndView handleAddEventForm(@Valid @ModelAttribute("event") EventWithoutImage eventWithoutImage, BindingResult br, @RequestParam("image") MultipartFile image) {
+        Event eventFromDb = eventFeignClient.getEventById(eventWithoutImage.getId());
         if (br.hasErrors()){
-            return "addEvent";
+            return showNewModelAndViewInCaseOfErrorsInAddOrEditEvents(eventFromDb, image, "addEvent");
         }
-        eventFeignClient.addEvent(event);
-        return "redirect:/events";
+        mapper.mapEventWithoutImageToEventWithMultipartImage(eventWithoutImage, image);
+        return new ModelAndView("redirect:/events");
     }
 
     @GetMapping("/{id}")
     public String displayEditEventForm(@PathVariable("id") long id, Model model){
         Event event = eventFeignClient.getEventById(id);
-        model.addAttribute("categoryList", eventFeignClient.getAllCategories().getCategories());
         model.addAttribute("event", event);
+        if(event.getImage() != null) {
+            model.addAttribute("eventImage", Base64.getEncoder().encodeToString(event.getImage()));
+        }
         return "editEvent";
     }
 
     @PostMapping("/{id}")
-    public String handleEditEventForm(@Valid @ModelAttribute("event") Event event, BindingResult br) {
+    public ModelAndView handleEditEventForm(@Valid @ModelAttribute("event") EventWithoutImage eventWithoutImage, BindingResult br, @RequestParam("image") MultipartFile image) {
+        Event eventFromDb = eventFeignClient.getEventById(eventWithoutImage.getId());
         if (br.hasErrors()){
-            return "editEvent";
+            return showNewModelAndViewInCaseOfErrorsInAddOrEditEvents(eventFromDb, image, "editEvent");
         }
+        Event event = mapper.mapEventWithoutImageToEventWithMultipartImage(eventWithoutImage, image);
         eventFeignClient.editEvent(event.getId(), event);
-        return "redirect:/events";
+        return new ModelAndView("redirect:/events");
+    }
+
+    private ModelAndView showNewModelAndViewInCaseOfErrorsInAddOrEditEvents(Event event, MultipartFile image, String viewName){
+        try {
+            if(image.getSize() != 0) {
+                String imageString = Base64.getEncoder().encodeToString(image.getBytes());
+                return new ModelAndView(viewName, "eventImage", imageString);
+            }
+            else if (event.getImage() != null){
+                String imageString = Base64.getEncoder().encodeToString(event.getImage());
+                return new ModelAndView(viewName, "eventImage", imageString);
+            }
+        } catch (IOException e) {
+            logger.error(String.format("Something went wrong when showing the new view for: %s", viewName));
+        }
+        return new ModelAndView(viewName);
     }
     
     @GetMapping("/search")
@@ -83,5 +114,10 @@ public class EventController {
             return "eventList";
         }
         return "searchForm";
+    }
+
+    @ModelAttribute (name = "categoryList")
+    public List<String> getCategories() {
+        return eventFeignClient.getAllCategories().getCategories();
     }
 }
