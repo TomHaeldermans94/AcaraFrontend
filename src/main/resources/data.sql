d7b55d88dc8489121957b202bc0057b475c73171
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-04-05 18:23:43 +0200

    Roles are working!

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
+insert into user(id, username, password) values (4,'michiel','$2a$09$NXc2Hlu9ng/rcjMHa4ggZO5J6ZXjZHtQZC4Ar360cJNIMVSW28K42');
 insert into user(id, username, password) values (5,'test','$2y$10$mUDfNQFM4HXQzgW98NXD6eS.CxHjVkRq2E20ZT.0YM2zJu1AsJblm');
