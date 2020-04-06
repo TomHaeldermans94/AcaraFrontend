f2c5c242f894962f2c981b0492a867b57812c745
TomHaeldermans94 <tomhaeldermans94@gmail.com>
2020-04-06 20:43:39 +0200

    AC-Layout-21 add image preview, add delete image button and clean up event form

--- src/main/java/be/acara/frontend/controller/EventController.java
+++ src/main/java/be/acara/frontend/controller/EventController.java
@@ -134,6 +134,11 @@ public class EventController {
         return "redirect:/events";
     }
 
+    @GetMapping(value="/image-removal")
+    public void imageRemoval(){
+        System.out.println("hoi");
+    }
+
     @ModelAttribute(name = "categoryList")
     public List<String> getCategories() {
         return eventFeignClient.getAllCategories().getCategories();
