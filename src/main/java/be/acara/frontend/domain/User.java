c5ffff98affb49ecd80e46dc9ae69b8a56115cb4
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-04-14 23:53:53 +0200

    Add EqualsAndHashcode.Exclude from Role and User

--- src/main/java/be/acara/frontend/domain/User.java
+++ src/main/java/be/acara/frontend/domain/User.java
@@ -3,7 +3,6 @@ package be.acara.frontend.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -21,26 +20,9 @@ public class User {
     @Transient
     private String passwordConfirm;
     @ManyToMany
+    @EqualsAndHashCode.Exclude
     private Set<Role> roles;
     private String firstName;
     private String lastName;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return Objects.equals(getId(), user.getId()) &&
-                Objects.equals(getUsername(), user.getUsername()) &&
-                Objects.equals(getPassword(), user.getPassword()) &&
-                Objects.equals(getPasswordConfirm(), user.getPasswordConfirm()) &&
-                Objects.equals(getFirstName(), user.getFirstName()) &&
-                Objects.equals(getLastName(), user.getLastName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getUsername(), getPassword(), getPasswordConfirm(), getFirstName(), getLastName());
-    }
 }
 
