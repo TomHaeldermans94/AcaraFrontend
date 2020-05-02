4eab5ac91336d9acd76cf2793868f06208d1802a
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-02 23:44:03 +0200

    Remove unused imports and unneeded methods

--- src/main/java/be/acara/frontend/service/OrderFeignClient.java
+++ src/main/java/be/acara/frontend/service/OrderFeignClient.java
@@ -1,12 +1,12 @@
 package be.acara.frontend.service;
 
 import be.acara.frontend.controller.dto.CreateOrderDto;
-import be.acara.frontend.controller.dto.EventDto;
-import be.acara.frontend.controller.dto.UserDto;
-import be.acara.frontend.domain.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(value = "orderFeignClient", url = "${baseURL}")
 public interface OrderFeignClient {
