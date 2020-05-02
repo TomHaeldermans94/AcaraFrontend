0c40793a05180e8263aa225451f2da04712095fd
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-02 22:40:59 +0200

    Add @SuppressWarnings to hide the warning that tells us to move constants out of interfaces. This technically isn't a constant..

--- src/main/java/be/acara/frontend/service/mapper/EventMapper.java
+++ src/main/java/be/acara/frontend/service/mapper/EventMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
+@SuppressWarnings("java:S1214") // remove the warning for the INSTANCE variable
 public interface EventMapper {
     
     EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
