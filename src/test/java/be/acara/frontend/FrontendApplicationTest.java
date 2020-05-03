425736a16f84c032a412395fd23a0ef54c79020a
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-05-03 02:49:27 +0200

    Add possibly horrible assertions to FrontendApplicationTest
    Add proper assertion to apply_withoutContext

--- src/test/java/be/acara/frontend/FrontendApplicationTest.java
+++ src/test/java/be/acara/frontend/FrontendApplicationTest.java
@@ -2,14 +2,25 @@ package be.acara.frontend;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FrontendApplicationTest {
     
     @Test
     void contextLoaded() {
+        try {
+            assertThat(true).isTrue();
+        } catch (Exception e) {
+            assertThat(true).isFalse();
+        }
     }
     
     @Test
     void applicationStart() {
-        FrontendApplication.main(new String[]{});
+        try {
+            FrontendApplication.main(new String[]{});
+        } catch (Exception e) {
+            assertThat(true).isFalse();
+        }
     }
 }
